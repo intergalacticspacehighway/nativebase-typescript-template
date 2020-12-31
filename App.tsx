204bb29121ccca22fa2466f92effa9dc899cf0f9
Nishan <nishanbende@gmail.com>
2020-12-31 13:11:47 +0530

    init

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Box, Link } from "native-base";
+
+export default function App() {
+  return (
+    <Box>
+      <Box>NativeBase 3.0</Box>
+      <Link href="https://nativebase.github.io/">Documentation</Link>
+    </Box>
+  );
+}
